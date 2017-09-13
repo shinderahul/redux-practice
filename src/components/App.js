53f448d816c6f3c6035ebca85840edcd326d022b
Rahul Shinde <rahshind@INMUBLBI0195.local>
2017-09-13 12:07:10 +0530

    Added components

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => (
+    <div>
+        <h2>User List</h2>
+        <hr />
+        <h2>User Details</h2>
+    </div>
+);
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
 import allReducers from './reducers';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 
 const store = createStore(allReducers);
 
-ReactDOM.render(<h2>Hello World</h2>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
