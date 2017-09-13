54ac2ec2fdd35972c3135cfe84e5aa6dd2ebc2a1
Rahul Shinde <rahshind@INMUBLBI0195.local>
2017-09-13 17:45:49 +0530

    Added link to repo

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import UserDetail from '../containers/UserDetail';
 
 const App = () => (
     <div className='container'>
+        <p><br />Prefer code <a href="https://github.com/shinderahul/redux-practice" target="_blank">here</a></p>
         <h2>User List</h2>
         <UserList />
         <hr />
