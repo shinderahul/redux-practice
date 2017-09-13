978d3f9dbf11f0b65a9257c5fda2bcfdca2827bf
Rahul Shinde <rahshind@INMUBLBI0195.local>
2017-09-13 15:00:35 +0530

    Actions and Action creators

--- src/containers/UserList.js
+++ src/containers/UserList.js
@@ -1,20 +1,36 @@
 import React, {Component} from 'react';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
+import {selectUser} from '../actions/';
 
 
 class UserList extends Component {
 
+  createListItems(){
+    return this.props.users.map((user) => {
+      return (
+        <li
+         key={user.id}
+         onClick={() => this.props.selectUser(user)}
+         >
+         {user.first} {user.last}
+         </li>
+      );
+    });
+  }
+
   render() {
     return (
       <ul>
-        <li>One</li>
-        <li>Two</li>
-        <li>Three</li>
+        {this.createListItems()}
       </ul>
     );
   }
 
+}
+
+function matchDispatchToProps(dispatch) {
+  return bindActionCreators({selectUser: selectUser}, dispatch);
 }
 
 function mapStateToProps(state) {
@@ -23,4 +39,4 @@ function mapStateToProps(state) {
   }
 }
 
-export default connect(mapStateToProps)(UserList);
+export default connect(mapStateToProps, matchDispatchToProps)(UserList);
